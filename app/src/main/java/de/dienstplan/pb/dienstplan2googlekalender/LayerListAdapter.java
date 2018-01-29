1a374d6903a4960c4de6ec51ef3f61e5b435c5ea
Borstentorsten <paule-mail@gmx.net>
2018-01-29 23:11:50 +0100

    addig layers and setting the start and endtimes

--- app/src/main/java/de/dienstplan/pb/dienstplan2googlekalender/LayerListAdapter.java
+++ app/src/main/java/de/dienstplan/pb/dienstplan2googlekalender/LayerListAdapter.java
@@ -1,8 +1,10 @@
 package de.dienstplan.pb.dienstplan2googlekalender;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.text.Layout;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,17 @@ import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.logging.ConsoleHandler;
 
 import de.dienstplan.pb.dienstplan2googlekalender.Business.LayerManager;
@@ -23,16 +33,23 @@ import de.dienstplan.pb.dienstplan2googlekalender.Model.Layer;
  * Created by Paule on 15.11.2017.
  */
 
-public class LayerListAdapter extends ArrayAdapter<Layer> {
+public class LayerListAdapter extends ArrayAdapter<Layer> implements View.OnClickListener, TimePickerDialog.OnTimeSetListener {
 
     Activity context;
     ArrayList<Layer> layers;
     LayerManager layerManager;
     Layer newEntry;
+    ListView listView;
 
-    public LayerListAdapter(Activity context, LayerManager layerManager) {
+    Layer currentLayerForTimePicker;
+    boolean isEditingStart;
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+
+    public LayerListAdapter(Activity context, LayerManager layerManager, ListView listView) {
         super(context, 0, layerManager.getLayerList());
         this.context = context;
+        this.listView = listView;
     }
 
     @Override
@@ -51,6 +68,26 @@ public class LayerListAdapter extends ArrayAdapter<Layer> {
         TextView textView = view.findViewById(R.id.editLayerName);
         Layer layer = getItem(i);
         textView.setText(layer.getName());
+        TextView textStart = view.findViewById(R.id.editLayerBegin);
+        TextView textEnd = view.findViewById(R.id.editLayerEnd);
+
+        Calendar start = layer.getStart();
+        Calendar end = layer.getEnd();
+        try {
+            if (start != null) {
+                textStart.setText(dateFormat.format(start.getTime()));
+            }
+            if (end != null) {
+                textEnd.setText(dateFormat.format(end.getTime()));
+            }
+        } catch (Exception exc) {
+            int x = 0;
+        }
+
+        textStart.setOnClickListener(this);
+        textStart.setTag(layer);
+        textEnd.setOnClickListener(this);
+        textEnd.setTag(layer);
         return view;
     }
 
@@ -58,5 +95,45 @@ public class LayerListAdapter extends ArrayAdapter<Layer> {
         newEntry = new Layer();
         newEntry.setName("Name");
         add(newEntry);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar date = new GregorianCalendar();
+        date.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        date.set(Calendar.MINUTE, minute);
+        if(isEditingStart) {
+            currentLayerForTimePicker.setStart(date);
+        }
+        else {
+            currentLayerForTimePicker.setEnd(date);
+        }
+        this.listView.invalidateViews();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Layer layer = (Layer)v.getTag();
+        currentLayerForTimePicker = layer;
+        Calendar date = null;
+        switch (v.getId()) {
+            case R.id.editLayerBegin:
+                date = layer.getStart();
+                isEditingStart = true;
+                break;
+            case R.id.editLayerEnd:
+                date = layer.getEnd();
+                isEditingStart = false;
+                break;
+        }
+        int hour = 0;
+        int min = 0;
+        if(date != null) {
+            hour = date.get(Calendar.HOUR_OF_DAY);
+            min = date.get(Calendar.MINUTE);
+        }
+        TimePickerDialog dlg = new TimePickerDialog(context, this,
+                hour, min, true);
+        dlg.show();
     }
 }
